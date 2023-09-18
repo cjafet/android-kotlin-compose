a6d5bbb9d43f5fb6e6fa813e0986d38775675171
cjafet <cjafet07@gmail.com>
2023-09-18 14:28:24 -0300

    Add Menu component

---
+++ app/src/main/java/br/com/fiap/composenavigation/components/Menu.kt
@@ -0,0 +1,37 @@
+package br.com.fiap.composenavigation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import br.com.fiap.composenavigation.R
+
+@Composable
+fun Menu() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Column(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .padding(top = 16.dp)) {
+            MenuItem(title = "Carteira", id = R.drawable.baseline_wallet_24)
+        }
+        Column(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .padding(top = 16.dp)) {
+            MenuItem(title = "Recomendações", id = R.drawable.baseline_bar_chart_24)
+        }
+        Column(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .padding(top = 16.dp)) {
+            MenuItem(title = "Artigos", id = R.drawable.baseline_article_24)
+        }
+    }
+}
