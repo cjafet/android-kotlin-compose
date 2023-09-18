63f439cf808d2704bf5e23ee3a96b7ffd1606b74
cjafet <cjafet07@gmail.com>
2023-09-18 14:25:22 -0300

    Add Recommendation model

---
+++ app/src/main/java/br/com/fiap/composenavigation/model/Recommendation.kt
@@ -0,0 +1,4 @@
+package br.com.fiap.composenavigation.model
+
+data class Recommendation(val id: Int, val stock: String, val closing: String, val potential: String, val min: String, val max:String) {
+}
