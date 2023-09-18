7ce44a7dde566055a32870ee2b530e51dac9e115
cjafet <cjafet07@gmail.com>
2023-09-18 14:30:56 -0300

    Add Recommendation to RecommendationCard

--- app/src/main/java/br/com/fiap/composenavigation/components/RecommendationCard.kt
+++ app/src/main/java/br/com/fiap/composenavigation/components/RecommendationCard.kt
@@ -1,10 +1,14 @@
 package br.com.fiap.composenavigation.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -12,20 +16,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.com.fiap.composenavigation.R
+import androidx.navigation.NavController
+import br.com.fiap.composenavigation.model.Recommendation
 
 @Composable
 fun RecommendationCard(
-    stock: String,
-    closing: String,
-    potential: String,
+    recommendation: Recommendation,
+    navController: NavController
 ) {
-
     Text(
-        text = stock,
+        text = recommendation.stock,
         fontSize = 22.sp,
         fontWeight = FontWeight.Bold,
         color = Color.White,
@@ -38,8 +42,8 @@ fun RecommendationCard(
             .padding(start = 24.dp, end = 24.dp)
             .height(120.dp),
         colors = CardDefaults
-            .cardColors(containerColor = Color(R.color.card)),
-        elevation = CardDefaults.cardElevation(8.dp)
+            .cardColors(containerColor = Color(0xFF2629B3)),
+        elevation = CardDefaults.cardElevation(12.dp)
     ) {
         Column(
             horizontalAlignment = Alignment.Start,
@@ -48,19 +52,38 @@ fun RecommendationCard(
                 .align(alignment = Alignment.Start)
                 .padding(start = 16.dp, top = 24.dp)
         ) {
-            Text(
-                text = "Fechamento: \t $closing",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
-            Spacer(Modifier.height(16.dp))
-            Text(
-                text = "Potencial: \t $potential%",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
+            Row {
+                Column(modifier = Modifier.width(180.dp).align(Alignment.CenterVertically)) {
+                    Text(
+                        text = "Fechamento: \t ${recommendation.closing}",
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Spacer(Modifier.height(16.dp))
+                    Text(
+                        text = "Potencial: \t ${recommendation.potential}%",
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Spacer(Modifier.height(8.dp))
+                }
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 30.dp)
+                ) {
+                    Button(
+                        onClick = { navController.navigate("detail/${recommendation.id}") },
+                        modifier = Modifier.align(Alignment.Start),
+                        shape = RectangleShape,
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent,Color.White),
+                    ) {
+                        Text(text = "+info", fontSize = 22.sp, color = Color.White)
+                    }
+                }
+            }
         }
     }
     Spacer(modifier = Modifier.height(18.dp))
